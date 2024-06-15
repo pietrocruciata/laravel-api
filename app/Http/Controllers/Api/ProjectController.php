262ef741c58db5c27f625ed1c2aa9c90f4634608
pietrocruciata <cruciata9748@gmail.com>
2024-06-15 21:01:55 +0200

    add: paginate

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\Api;
+
 use App\Models\Project;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
@@ -9,13 +10,15 @@ class ProjectController extends Controller
 {
     public function index(Request $request)
     {
-        $projects = Project::all();
-        
+        // $projects = Project::all();
+        $projects = Project::with('types', 'technologies')->paginate(3);
+
 
 
         return response()->json([
-            'projects' => $projects,
-     
+            // 'projects' => $projects,
+            'projects' => $projects
+
         ]);
     }
 }
